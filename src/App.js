003b86376baa5523fd72722973df89884d64f4e0
ega <akira.ega@gmail.com>
2019-08-03 10:07:44 +0900

    add lanuch.json for debugging with vscode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React, { Component } from "react";
+import React, {
+  Component
+} from "react";
 import Navbar from "./components/Navbar";
 import Custom from "./components/Custom";
 import "./custom.css";
+
 class App extends Component {
   render() {
-    return (
-      <div>
-        <Navbar />
-        <Custom />
-      </div>
+    return ( <
+      div >
+      <
+      Navbar / >
+      <
+      Custom / >
+      <
+      /div>
     );
   }
 }
