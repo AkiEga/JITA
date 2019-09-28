1084c7195de913320763904683bac6473c259876
ega <akira.ega@gmail.com>
2019-09-28 15:46:32 +0900

    update parsejson and eslint-utils

--- src/components/Browser/index.js
+++ src/components/Browser/index.js
@@ -1,18 +1,44 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import {
+  Icon,
+  Accordion,
+} from 'semantic-ui-react'
+
 
 export class Browser extends React.Component {
-  constructor(props) {
+  constructor(props){
     super(props)
+    this.state = { activeIndex: 0 }
+    this.handleClick = this.handleClick.bind(this)
   }
+  handleClick(e, titleProps){
+    const { index } = titleProps
+    const { activeIndex } = this.state
+    const newIndex = activeIndex === index ? -1 : index
+
+    this.setState({ activeIndex: newIndex })
+  }  
   render() {    
+    const { activeIndex } = this.state
     let url = this.props.url
-    return <webview 
-    src={url} 
-    autosize="on" 
-    style={{height: '-webkit-fill-available'}}>
-    </webview>
-    
+    return <Accordion>        
+      <Accordion.Title
+        index={0}
+        onClick={this.handleClick}
+      >
+      <Icon name='dropdown' />
+      Browser
+      </Accordion.Title>
+      <Accordion.Content >
+      <webview 
+      src={url} 
+      autosize="on" 
+      style={{height: '-webkit-fill-available'}}>
+      </webview>
+      
+      </Accordion.Content>        
+    </Accordion>
   }
 }
 
--- src/electron.js
+++ src/electron.js
@@ -12,8 +12,8 @@ process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = true;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
-    // width: 900,
-    // height: 680,
+    width: 1280,
+    height: 960,
     webPreferences: {
       nodeIntegration: true
     }
